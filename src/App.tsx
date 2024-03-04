cc5114e126511eb66a2b38a8bfcb0098ccdf31f4
Martin Macharia <mosmartin5@gmail.com>
2024-03-04 10:57:33 +0100

    Refactor handleAddToFeedbackList and add postFeedback function

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,6 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const handleAddToFeedbackList = (text: string) => {
-    const comapanyName =
-      text
-        .split(" ")
-        .find((word) => word.includes("#"))
-        ?.substring(1) ?? "";
-
-    const feedback: TFeedbackItem = {
-      id: new Date().getTime().toString(),
-      upvoteCount: 0,
-      badgeLetter: comapanyName.substring(0, 1).toUpperCase(),
-      company: comapanyName,
-      text,
-      daysAgo: 0,
-    };
-
-    setFeedbackItems([...feedbackItems, feedback]);
-  };
-
   const fetchFeedbacks = async () => {
     setIsLoading(true);
 
@@ -48,6 +29,56 @@ function App() {
       setErrorMessage("Error fetching feedbacks");
       setIsLoading(false);
     }
+  };
+
+  const postFeedback = async (feedback: TFeedbackItem) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(feedback),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Error posting feedback!");
+      }
+
+      const data = await response.json();
+
+      setFeedbackItems([...feedbackItems, data.feedback]);
+      setIsLoading(false);
+    } catch (error) {
+      setErrorMessage("Error posting feedback");
+      setIsLoading(false);
+    }
+  };
+
+  const handleAddToFeedbackList = async (text: string) => {
+    const comapanyName =
+      text
+        .split(" ")
+        .find((word) => word.includes("#"))
+        ?.substring(1) ?? "";
+
+    const feedback: TFeedbackItem = {
+      id: new Date().getTime().toString(),
+      upvoteCount: 0,
+      badgeLetter: comapanyName.substring(0, 1).toUpperCase(),
+      company: comapanyName,
+      text,
+      daysAgo: 0,
+    };
+
+    setFeedbackItems([...feedbackItems, feedback]);
+
+    await postFeedback(feedback);
   };
 
   useEffect(() => {
